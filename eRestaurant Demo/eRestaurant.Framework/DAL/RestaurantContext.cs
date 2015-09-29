61ae9482ed89a2d5da822559f81e4e795c6a495a
gnguyen7 <gnguyen7@studentmail.nait.ca>
2015-09-29 15:28:57 -0600

    E-Restaurant

---
+++ eRestaurant Demo/eRestaurant.Framework/DAL/RestaurantContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using eRestaurant.Framework.Entities; //Need for Entity class
+using System.Data.Entity; //Need for DbContext and other EF class
+
+namespace eRestaurant.Framework.DAL
+{
+    //By making our DAL class internal, that mean outside projects can't directly
+    //access our Data Access Layer (they have to go through our BLL to do stuff)
+    internal class RestaurantContext : DbContext
+    {
+        public RestaurantContext()
+            : base("DefaultConnection")
+        { }
+        //One property for each Table/Entity in the database
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        //public DbSet<Bill> Bills { get; set; }
+        //public DbSet<BillItem> BillItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<MenuCategory> MenuCategories { get; set; }
+        //public DbSet<Waiter> Waiters { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Reservation>().HasMany(r => r.Tables)
+                .WithMany(t => t.Reservations)
+                .Map(mapping =>
+                {
+                    mapping.ToTable("ReservationTables");
+                    mapping.MapLeftKey("ReservationID");
+                    mapping.MapRightKey("TableID");
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Reservation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Framework.Entities
+{
+    public class Reservation
+    {
+        // constants for valid reservation status values
+        // B = Booked; Arrived = "A"; C = Complete; N = No-show; X = Cancelled
+        public const string Booked = "B";
+        public const string Arrived = "A";
+        public const string Complete = "C";
+        public const string NoShow = "N";
+        public const string Cancelled = "X";
+
+        public int ReservationID { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CustomerName { get; set; }
+        public DateTime ReservationDate { get; set; }
+        [Range(1, 16)]
+        public int NumberInParty { get; set; }
+        [StringLength(15)]
+        public string ContactPhone { get; set; }
+        // TODO: (in BLL) Validate acceptable values using constants above
+        [Required, StringLength(1, MinimumLength = 1)]
+        public string ReservationStatus { get; set; }
+        [StringLength(1)]
+        public string EventCode { get; set; }
+
+        public virtual SpecialEvent Event { get; set; }
+        public virtual ICollection<Table> Tables { get; set; }
+        //public virtual ICollection<Bill> Bills { get; set; }
+    }
+}
