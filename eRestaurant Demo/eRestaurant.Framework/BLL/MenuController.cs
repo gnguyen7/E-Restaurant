32487bcff8d64706e69d320495a331a9dfa5a986
gnguyen7 <gnguyen7@studentmail.nait.ca>
2015-10-13 16:04:48 -0600

    MenuController

---
+++ eRestaurant Demo/eRestaurant.Framework/BLL/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel; // for [DataObject] et.al attributes
+using eRestaurant.Framework.DAL; // for RestaurantContext
+using eRestaurant.Framework.Entities; //for DB Entity Classes
+
+namespace eRestaurant.Framework.BLL
+{
+    [DataObject]
+    public class MenuController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Item> ListMenuItem()
+        {
+            using (var context = new RestaurantContext())
+            {
+                return context.Items.ToList();
+            }
+        }
+    }
+}
--- eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
+++ eRestaurant Demo/eRestaurant.Framework/Entities/Item.cs
@@ -12,8 +12,8 @@ namespace eRestaurant.Framework.Entities
         [Key]
         public int ItemID { get; set; }
         public string Description { get; set; }
-        public double CurrentPrice { get; set; }
-        public double CurrentCost { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal CurrentCost { get; set; }
         public bool Active { get; set; }
         public int? Calories { get; set; }
         public string Comment { get; set; }
